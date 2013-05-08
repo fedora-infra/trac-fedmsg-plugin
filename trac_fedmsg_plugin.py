f70476587c5c87b5f15a5826ff08ba8ca4bc11d0
Ralph Bean <rbean@redhat.com>
2013-05-07 20:47:06 -0400

    Some cleanup.

--- trac_fedmsg_plugin.py
+++ trac_fedmsg_plugin.py
@@ -10,6 +10,17 @@ if not getattr(getattr(fedmsg, '__local', None), '__context', None):
     fedmsg.init(name="trac." + hostname)
 
 
+def env2dict(env):
+    """ Utility method to format the trac environment for fedmsg. """
+    return dict(
+        base_url=env.base_url,
+        project_name=env.project_name,
+        project_description=env.project_description,
+        project_url=env.project_url,
+        project_icon=env.project_icon,
+    )
+
+
 class FedmsgPlugin(trac.core.Component):
     """ The trac fedmsg plugin.
 
@@ -20,23 +31,21 @@ class FedmsgPlugin(trac.core.Component):
         trac.ticket.api.ITicketChangeListener,
     )
 
-    def env2dict(self):
-        return dict(
-            base_url=self.env.base_url,
-            project_name=self.env.project_name,
-            project_description=self.env.project_description,
-            project_url=self.env.project_url,
-            project_icon=self.env.project_icon,
-        )
-
     def publish(self, topic, **msg):
-        msg['instance'] = self.env2dict()
+        """ Inner workhorse method.  Publish arguments to fedmsg. """
+        msg['instance'] = env2dict(self.env)
         fedmsg.publish(modname='trac', topic=topic, msg=msg)
 
     def ticket_created(self, ticket):
+        """Called when a ticket is created."""
         self.publish(topic='ticket.new', ticket=ticket.values)
 
     def ticket_changed(self, ticket, comment, author, old_values):
+        """Called when a ticket is modified.
+
+        `old_values` is a dictionary containing the previous values of the
+        fields that have changed.
+        """
         self.publish(
             topic='ticket.update',
             ticket=ticket.values,
@@ -46,4 +55,5 @@ class FedmsgPlugin(trac.core.Component):
         )
 
     def ticket_deleted(self, ticket):
+        """Called when a ticket is deleted."""
         self.publish(topic='ticket.delete', ticket=ticket.values)
