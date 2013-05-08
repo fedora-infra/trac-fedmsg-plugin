e8cbb539247b0ffef6ffa7e7f9cba670bb03e95d
Ralph Bean <rbean@redhat.com>
2013-05-07 20:47:27 -0400

    Support for wiki edits and revision control.

--- trac_fedmsg_plugin.py
+++ trac_fedmsg_plugin.py
@@ -1,5 +1,8 @@
 import trac.core
+
 import trac.ticket.api
+import trac.wiki.api
+import trac.versioncontrol.api
 
 import socket
 import fedmsg
@@ -21,6 +24,11 @@ def env2dict(env):
     )
 
 
+def wikipage2dict(page):
+    attrs = ['version', 'time', 'author', 'text', 'comment', 'readonly']
+    return dict([(attr, getattr(page, attr)) for attr in attrs])
+
+
 class FedmsgPlugin(trac.core.Component):
     """ The trac fedmsg plugin.
 
@@ -29,6 +37,8 @@ class FedmsgPlugin(trac.core.Component):
     """
     trac.core.implements(
         trac.ticket.api.ITicketChangeListener,
+        trac.wiki.api.IWikiChangeListener,
+        trac.versioncontrol.api.IRepositoryChangeListener,
     )
 
     def publish(self, topic, **msg):
@@ -57,3 +67,58 @@ class FedmsgPlugin(trac.core.Component):
     def ticket_deleted(self, ticket):
         """Called when a ticket is deleted."""
         self.publish(topic='ticket.delete', ticket=ticket.values)
+
+    def wiki_page_added(self, page):
+        """Called whenever a new Wiki page is added."""
+        self.publish(topic='wiki.page.new', page=wikipage2dict(page))
+
+    def wiki_page_changed(self, page, version, t, comment, author, ipnr):
+        """Called when a page has been modified."""
+        self.publish(
+            topic='wiki.page.update',
+            page=wikipage2dict(page),
+            version=version,
+            t=t,
+            comment=comment,
+            author=author,
+            #ipnr=ipnr,  # Don't broadcast IP addresses.  Poor form.
+        )
+
+    def wiki_page_deleted(self, page):
+        """Called when a page has been deleted."""
+        self.publish(topic='wiki.page.delete', page=wikipage2dict(page))
+
+    def wiki_page_version_deleted(self, page):
+        """Called when a version of a page has been deleted."""
+        self.publish(topic='wiki.page.version.delete',
+                     page=wikipage2dict(page))
+
+    def wiki_page_renamed(self, page, old_name):
+        """Called when a page has been renamed."""
+        self.publish(
+            topic='wiki.page.rename',
+            page=wikipage2dict(page),
+            old_name=old_name
+        )
+
+    def changeset_added(self, repos, changeset):
+        """Called after a changeset has been added to a repository."""
+        self.publish(
+            topic='changeset.new',
+            repos=[repo.name for repo in repos],
+            changeset=changeset.get_properties(),
+        )
+
+    def changeset_modified(self, repos, changeset, old_changeset):
+        """Called after a changeset has been modified in a repository.
+
+        The `old_changeset` argument contains the metadata of the changeset
+        prior to the modification. It is `None` if the old metadata cannot
+        be retrieved.
+        """
+        self.publish(
+            topic='changeset.update',
+            repos=[repo.name for repo in repos],
+            changeset=changeset.get_properties(),
+            old_changeset=old_changeset.get_properties(),
+        )
